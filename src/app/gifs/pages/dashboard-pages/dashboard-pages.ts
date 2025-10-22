2389c78e7e25a5aff459eea7ce0b5a74756d5dd7
JRamses11 <ramchu_aldama@hotmail.com>
2025-10-22 15:27:49 -0700

    Pages dashboard, search y trending

---
+++ src/app/gifs/pages/dashboard-pages/dashboard-pages.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-pages.html',
+  styleUrls: ['./dashboard-pages.css']
+})
+export default class DashboardPages {
+
+}
---
+++ src/app/gifs/pages/trending-pages/trending-pages.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-pages',
+  imports: [],
+  templateUrl: './trending-pages.html',
+  styleUrl: './trending-pages.css'
+})
+export default class TrendingPages {
+
+}
