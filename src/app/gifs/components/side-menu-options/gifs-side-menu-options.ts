d7acd65921266d1389cf92d43121a50ee94d1895
JRamses11 <ramchu_aldama@hotmail.com>
2025-10-27 20:28:08 -0700

    Router link actives para el menu

--- src/app/gifs/components/side-menu-options/gifs-side-menu-options.ts
+++ src/app/gifs/components/side-menu-options/gifs-side-menu-options.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions {
+  label: string;
+  sublabel: string;
+  router: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
   styleUrl: './gifs-side-menu-options.css'
 })
 export class GifsSideMenuOptions {
+
+  menuOption: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs populares',
+      router: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscar',
+      sublabel: 'Busca tus gifs',
+      router: '/dashboard/search'
+    },
+  ];
 
 }
