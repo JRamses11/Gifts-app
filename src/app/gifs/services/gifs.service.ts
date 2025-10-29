090b0c4382a26db494172ba19fe4b5ad83173f90
JRamses11 <ramchu_aldama@hotmail.com>
2025-10-29 14:35:29 -0700

    Peticion get para la pantalla de trending gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,18 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import type { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
     private http =inject(HttpClient);
+
+    trendingGifs= signal<Gif[]>([]);
+    trendingGifsLoading= signal(true);
 
     constructor() {
         this.loadTrendingGifs();
@@ -21,6 +26,11 @@ export class GifsService {
             api_key: environment.apiKeyGifs,
             limit: '25',
         }
+    }).subscribe((res) => {
+        const gifs= GifMapper.mapGiphyItemToGifArray(res.data);
+        this.trendingGifs.set(gifs);
+        this.trendingGifsLoading.set(false);
+        console.log(gifs);
     });
   }
 }
