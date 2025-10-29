57e3f41f21999d7cb74c2958d2c8038839accd02
JRamses11 <ramchu_aldama@hotmail.com>
2025-10-28 20:43:13 -0700

    Environments para apiKeys de gifs, actualizacion del readme

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,8 @@ export const environment = {
     production: true,
     companyName: 'Gifs',
     companyName2: 'App',
-    companySlogan: 'Maneja tus Gifs'
+    companySlogan: 'Maneja tus Gifs',
+
+    //API KEY GIPHY
+    apiKeyGifs: 'SruI9Lmog78EhCsrFleshtYtiNxwMbEs'
 };
